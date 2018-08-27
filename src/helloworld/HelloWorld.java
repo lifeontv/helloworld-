2fe925866ac5e93d0ed8bc880a4d90fdb7847d2c
computer <computer@DESKTOP-UPI0JGQ>
2018-08-27 16:19:04 +0800

    suibiangaigai

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("hello world随便改一改");
     }
 }
